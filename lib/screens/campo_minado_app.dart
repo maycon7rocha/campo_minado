6357a9d97e1ef4c0be5c896119e028409e6d9b12
Maycon Rocha <maycon7rocha@gmail.com>
2021-02-25 13:04:08 -0300

    410

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,12 +1,19 @@
+import 'package:campo_minado/components/resultado_widget.dart';
 import 'package:flutter/material.dart';
 
 class CampoMinadoApp extends StatelessWidget {
+
+  _reiniciar(){
+    print('reiniciou');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Campo Minado'),
+        appBar: ResultadoWidget(
+          venceu: null,
+          onReiniciar: _reiniciar,
         ),
         body: Container(
           child: Text('Tabuleiro'),
