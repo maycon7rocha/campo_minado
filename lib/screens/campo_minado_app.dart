03fea26fbef22ab68e7684e5ef73a341693156f6
Maycon Rocha <maycon7rocha@gmail.com>
2021-02-26 11:57:28 -0300

    416

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -13,11 +13,7 @@ class CampoMinadoApp extends StatefulWidget {
 class _CampoMinadoAppState extends State<CampoMinadoApp> {
   bool _venceu;
 
-  Tabuleiro _tabuleiro = Tabuleiro(
-    linhas: 12,
-    colunas: 12,
-    qtdeBombas: 3,
-  );
+  Tabuleiro _tabuleiro;
 
   void _reiniciar() {
     setState(() {
@@ -48,13 +44,28 @@ class _CampoMinadoAppState extends State<CampoMinadoApp> {
     if (_venceu != null) {
       return;
     }
-    
+
     setState(() {
       campo.alternarMarcacao();
       if (_tabuleiro.resolvido) {
         _venceu = true;
       }
     });
+  }
+
+  Tabuleiro _getTabuleiro(double largura, double altura) {
+    if (_tabuleiro == null) {
+      int qtdeColunas = 15;
+      double tamanhoCampo = largura / qtdeColunas;
+      int qtdeLinhas = (altura / tamanhoCampo).floor();
+
+      _tabuleiro = Tabuleiro(
+        linhas: qtdeLinhas,
+        colunas: qtdeColunas,
+        qtdeBombas: 50,
+      );
+    }
+    return _tabuleiro;
   }
 
   @override
@@ -66,10 +77,18 @@ class _CampoMinadoAppState extends State<CampoMinadoApp> {
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: TabuleiroWidget(
-            tabuleiro: _tabuleiro,
-            onAbrir: _abrir,
-            onAlternarMarcacao: _alternarMarcacao,
+          color: Colors.grey,
+          child: LayoutBuilder(
+            builder: (ctx, constraints) {
+              return TabuleiroWidget(
+                tabuleiro: _getTabuleiro(
+                  constraints.maxWidth,
+                  constraints.maxHeight,
+                ),
+                onAbrir: _abrir,
+                onAlternarMarcacao: _alternarMarcacao,
+              );
+            },
           ),
         ),
       ),
